cff0c7b668eac27798883106bb0a3d6b43cf8389
timmy <timmy56887@icloud.com>
2025-01-15 16:20:40 +0800

    1.0.9

--- addon/fold/indent-fold.js
+++ addon/fold/indent-fold.js
@@ -2,7 +2,12 @@
 // Distributed under an MIT license: https://codemirror.net/5/LICENSE
 
 (function(mod) {
-  mod(window.CodeMirror)
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
 })(function(CodeMirror) {
 "use strict";
 
